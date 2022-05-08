1f4dd7c4ebdd774e7e321f6d99dc229fcca81cd2
boutre <boutre002@gmail.com>
2022-05-08 15:17:31 +0200

    premier livrable backend

--- Backend/routes/reset.js
+++
@@ -1,9 +0,0 @@
-const express= require('express');
-const router = express.Router();
-const resetCtrl = require('../controllers/reset')
-
-//router.post('/', resetCtrl.ResetPassword);
-//router.post('/newPass', resetCtrl.NewPassword);
-//router.post('/', resetCtrl.ValidPasswordToken);
-
-  module.exports = router;
